a9130d68aabdb22e5ad148f2d437574ad7718850
ivythebest69 <1546528066@qq.com>
2026-08-27 23:56:46 +0800

    Update index.js

--- directory/frontend/src/router/index.js
+++ directory/frontend/src/router/index.js
@@ -1,14 +1,25 @@
 // frontend/src/router/index.js
 
+
 import { createRouter, createWebHistory } from "vue-router"
 
 
+
 // 页面组件
+
 import Product from "../views/Product.vue"
+
+import Order from "../views/Order.vue"
+
+
 
 
 
 const routes = [
+
+
+
+    // 默认进入商品管理
 
     {
         path: "/",
@@ -16,23 +27,45 @@ const routes = [
     },
 
 
+
+    // 商品管理
+
     {
         path: "/product",
         name: "Product",
         component: Product
+    },
+
+
+
+    // 订单管理
+
+    {
+        path: "/order",
+        name: "Order",
+        component: Order
     }
+
+
 
 ]
 
 
 
+
+
 const router = createRouter({
+
 
     history: createWebHistory(),
 
+
     routes
 
+
 })
+
+
 
 
 
