811f6b16a752bd391025a4a03ed865eff3326d26
ivythebest69 <1546528066@qq.com>
2026-08-28 00:16:20 +0800

    Update __init__.py

--- directory/backend/models/__init__.py
+++ directory/backend/models/__init__.py
@@ -1,9 +1,26 @@
-from .admin import Admin
-from .user import User
-from .category import Category
-from .brand import Brand
-from .order import Order
-from .after_sale import AfterSale
-from .operation_log import OperationLog
+# backend/models/__init__.py
 
+
+from .admin import Admin
+
+from .user import User
+
+from .category import Category
+
+from .brand import Brand
+
+
+# 商品模块
 from .product import Product
+
+
+# 订单模块
+from .order import Order
+
+
+# 售后模块
+from .after_sale import AfterSale
+
+
+# 操作日志
+from .operation_log import OperationLog
