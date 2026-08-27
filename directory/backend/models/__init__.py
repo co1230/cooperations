7e9dc364c60d63d2892cb7bd401b1e27fdbb9400
ivythebest69 <1546528066@qq.com>
2026-08-27 16:07:26 +0800

    Create __init__.py

---
+++ directory/backend/models/__init__.py
@@ -0,0 +1,10 @@
+from .admin import Admin
+from .user import User
+from .category import Category
+from .brand import Brand
+from .order import Order
+from .after_sale import AfterSale
+from .operation_log import OperationLog
+
+# 成员D新增商品模型
+from .product import Product
